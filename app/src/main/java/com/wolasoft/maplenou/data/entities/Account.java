7d81c1e4bbd401f851b0b0d88ce796af3820ad60
wolasoft <dev.wolasoft@gmail.com>
2019-05-01 10:58:26 +0200

    feat: add account entities

---
+++ app/src/main/java/com/wolasoft/maplenou/data/entities/Account.java
@@ -0,0 +1,17 @@
+package com.wolasoft.maplenou.data.entities;
+
+import java.util.Date;
+
+public class Account {
+    private String uuid;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private Date lastLogin;
+    private boolean certified;
+    private String status;
+    private Date creationDate;
+    private Date modificationDate;
+    private Date deletionDate;
+    private Person person;
+}
