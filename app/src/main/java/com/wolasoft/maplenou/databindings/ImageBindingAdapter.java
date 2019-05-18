35dcd135207abc7140b8f4f84f03e09e39c2184e
wolasoft <dev.wolasoft@gmail.com>
2019-05-18 21:35:53 +0200

    feat: enhance image binding adapter

--- app/src/main/java/com/wolasoft/maplenou/databindings/ImageBindingAdapter.java
+++ app/src/main/java/com/wolasoft/maplenou/databindings/ImageBindingAdapter.java
@@ -2,13 +2,16 @@ package com.wolasoft.maplenou.databindings;
 
 import android.widget.ImageView;
 
+import androidx.databinding.BindingAdapter;
+
 import com.squareup.picasso.Picasso;
 import com.wolasoft.maplenou.R;
 import com.wolasoft.maplenou.data.entities.Photo;
+import com.wolasoft.maplenou.utils.Constants;
+import com.wolasoft.waul.utils.ImageUtils;
 
+import java.io.File;
 import java.util.List;
-
-import androidx.databinding.BindingAdapter;
 
 public class ImageBindingAdapter {
 
@@ -20,15 +23,39 @@ public class ImageBindingAdapter {
             if (imageUrl != null && !imageUrl.equals("")) {
                 Picasso.get()
                         .load(imageUrl)
-                        .error(R.drawable.ic_photo_camera_black_24dp)
-                        .placeholder(R.drawable.ic_photo_camera_black_24dp)
+                        .error(R.drawable.no_image)
+                        .placeholder(R.drawable.no_image)
                         .into(view);
             } else {
                 view.setImageDrawable(
-                        view.getContext().getResources().getDrawable(R.drawable.ic_photo_camera_black_24dp));
+                        view.getContext()
+                                .getResources().getDrawable(R.drawable.no_image));
             }
         } else {
-            view.setImageResource(R.drawable.ic_photo_camera_black_24dp);
+            view.setImageResource(R.drawable.no_image);
+        }
+    }
+
+    @BindingAdapter({"app:localImageUrl"})
+    public static void loadLocalImage(ImageView view, List<Photo> photos) {
+        if (photos != null) {
+            String imageName = photos.get(0).getFile();
+
+            if (imageName != null && !imageName.equals("")) {
+                File file = ImageUtils.loadImageFromDisk(
+                        view.getContext(), Constants.LOCAL_IMAGE_DIR, imageName);
+                Picasso.get()
+                        .load(file)
+                        .error(R.drawable.no_image)
+                        .placeholder(R.drawable.no_image)
+                        .into(view);
+            } else {
+                view.setImageDrawable(
+                        view.getContext()
+                                .getResources().getDrawable(R.drawable.no_image));
+            }
+        } else {
+            view.setImageResource(R.drawable.no_image);
         }
     }
 
