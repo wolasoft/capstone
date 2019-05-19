f1b36c86bf396068e144fa9cbd97edbb889e8a41
wolasoft <dev.wolasoft@gmail.com>
2019-05-19 16:15:59 +0200

    chore: add account announcements service route

--- app/src/main/java/com/wolasoft/maplenou/data/api/services/AccountService.java
+++ app/src/main/java/com/wolasoft/maplenou/data/api/services/AccountService.java
@@ -1,17 +1,24 @@
 package com.wolasoft.maplenou.data.api.services;
 
 import com.google.gson.JsonObject;
+import com.wolasoft.maplenou.data.api.ApiResponse;
 import com.wolasoft.maplenou.data.entities.Account;
+import com.wolasoft.maplenou.data.entities.Announcement;
 import com.wolasoft.maplenou.data.entities.Token;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface AccountService {
     @POST("accounts")
     Call<Token> create(@Body JsonObject object);
     @GET("accounts")
     Call<Account> getOne();
+    @GET("accounts/{uuid}/announcements")
+    Call<ApiResponse<Announcement>> getAnnouncements(
+            @Path ("uuid") String uuid, @Query("page") int page);
 }
--- app/src/main/java/com/wolasoft/maplenou/data/repositories/IAccountRepository.java
+++ app/src/main/java/com/wolasoft/maplenou/data/repositories/IAccountRepository.java
@@ -1,14 +1,19 @@
 package com.wolasoft.maplenou.data.repositories;
 
+import com.wolasoft.maplenou.data.api.ApiResponse;
 import com.wolasoft.maplenou.data.api.services.CallBack;
 import com.wolasoft.maplenou.data.entities.Account;
+import com.wolasoft.maplenou.data.entities.Announcement;
 import com.wolasoft.maplenou.data.entities.Token;
+
+import retrofit2.Call;
 
 public interface IAccountRepository {
     void create(
             String email, String phoneNumber, String password, String lastName, String firstName,
             CallBack<Token> callBack);
     void getOne(CallBack<Account> callBack);
+    Call<ApiResponse<Announcement>> getAnnouncements(String uuid, final int page);
     void saveToken(Token token);
     void clearUserData();
     Account getAccount();
