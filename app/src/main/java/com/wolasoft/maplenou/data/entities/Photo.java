dfc7b80b519a7a82e02262e8b47061ca221cc7d4
wolasoft <dev.wolasoft@gmail.com>
2019-05-21 03:22:15 +0200

    chore: make photo model parcelable

--- app/src/main/java/com/wolasoft/maplenou/data/entities/Photo.java
+++ app/src/main/java/com/wolasoft/maplenou/data/entities/Photo.java
@@ -1,8 +1,11 @@
 package com.wolasoft.maplenou.data.entities;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 
-public class Photo {
+public class Photo implements Parcelable {
     private int id;
     private String uuid;
     private String mimetype;
@@ -11,6 +14,40 @@ public class Photo {
     private Date creationDate;
     private Date modificationDate;
     private Date deletionDate;
+
+    protected Photo(Parcel in) {
+        id = in.readInt();
+        uuid = in.readString();
+        mimetype = in.readString();
+        file = in.readString();
+        status = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(uuid);
+        dest.writeString(mimetype);
+        dest.writeString(file);
+        dest.writeString(status);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Photo> CREATOR = new Creator<Photo>() {
+        @Override
+        public Photo createFromParcel(Parcel in) {
+            return new Photo(in);
+        }
+
+        @Override
+        public Photo[] newArray(int size) {
+            return new Photo[size];
+        }
+    };
 
     public int getId() {
         return id;
